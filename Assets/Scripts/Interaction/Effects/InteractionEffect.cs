e249c1d6f8b72ada1247f272a316fa62c58a0711
CoreyEStinson <141242425+CoreyEStinson@users.noreply.github.com>
2026-07-12 23:54:05 -0400

    Revert "Add interaction system"
    This reverts commit f48503a280d1cebde8abbce1ad0fc66a61c5d4cf.

--- Assets/Scripts/Interaction/Effects/InteractionEffect.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class InteractionEffect : ScriptableObject
-{
-    [SerializeField] private string debugName;
-
-    public abstract void Execute(InteractionContext context);
-}
--- Assets/Scripts/Interaction/IInteractable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface IInteractable
-{
-    bool CanInteract(InteractionContext context, out string failureReason);
-    string GetPrompt(InteractionContext context);
-    Vector3 GetInteractionPoint();
-    void Interact(InteractionContext context);
-}
